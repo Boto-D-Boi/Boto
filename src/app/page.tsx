ede0e5abcd47e9b33073d0845d184ec0cde9d934
Davyn Ogawa <ogawad808@gmail.com>
2024-11-25 08:57:55 -1000

    pd1pgr2do

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { db } from "~/server/db";
+import { getMyImages } from "~/server/queries"
 
 /*const mockUrls =[
 
@@ -12,11 +13,14 @@ const mockImages = mockUrls.map((url, index) => ({
 
 
 export default async function HomePage() {
-  const images = await db.query.images.findMany();
+  const images = await getMyImages().catch((error) => {
+    console.error(error);
+    return null;
+  });
   return (
     <main className="">
       <div className="flex flex-wrap gap-4 text-orange-600 text-lg tracking-wider border-8 border-yellow-600 bg-blue-200">
-        {[...images].map((image) => (
+        {images !=null && images.map(image, index) => (
           <div key={image.id} className="w-48 border-8 border-l-green-500 border-b-orange-600 border-t-pink-600 border-r-blue-600 brightness-100 bg-violet-700">
             <img src={image.url} />
             <div>{image.name}</div>
